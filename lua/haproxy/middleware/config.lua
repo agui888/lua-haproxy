5f60de380d0d9839f6855a7d4d422f384002adb2
Ben Webber <benjamin.webber@gmail.com>
2016-06-18 23:18:17 -0400

    Extract process inspection into separate module

--- lua/haproxy/middleware/config.lua
+++ lua/haproxy/middleware/config.lua
@@ -1,37 +1,9 @@
-function get_cmdline()
-  local f = assert(io.open('/proc/self/cmdline', 'rb'))
-  local s = f:read('*a')
-  f:close()
-  return s
-end
-
-function get_config()
-  local cmdline = get_cmdline()
-  local files = {}
-  local token, previous
-  local remainder = false
-  for token in string.gmatch(cmdline, '%g+') do
-    -- -- signifies that the remaining arguments are configuration files.
-    if token == '--' then
-      remainder = true
-    elseif previous == '-f' or remainder then
-      files[#files+1] = token
-    end
-    previous = token
-  end
-  local config = ''
-  for _, file in ipairs(files) do
-    local f = assert(io.open(file, 'r'))
-    config = config .. f:read('*a')
-    f:close()
-  end
-  return config
-end
+local process = require('haproxy.process')
 
 local config
 
 core.register_init(function()
-  config = get_config()
+  config = process.config()
 end)
 
 core.register_service('config', 'http', function(applet)
---
+++ lua/haproxy/process.lua
@@ -0,0 +1,33 @@
+local M = {}
+
+function M.cmdline()
+  local f = assert(io.open('/proc/self/cmdline', 'rb'))
+  local s = f:read('*a')
+  f:close()
+  return s
+end
+
+function M.config()
+  local cmdline = M.cmdline()
+  local files = {}
+  local token, previous
+  local remainder = false
+  for token in string.gmatch(cmdline, '%g+') do
+    -- -- signifies that the remaining arguments are configuration files.
+    if token == '--' then
+      remainder = true
+    elseif previous == '-f' or remainder then
+      files[#files+1] = token
+    end
+    previous = token
+  end
+  local config = ''
+  for _, file in ipairs(files) do
+    local f = assert(io.open(file, 'r'))
+    config = config .. f:read('*a')
+    f:close()
+  end
+  return config
+end
+
+return M
