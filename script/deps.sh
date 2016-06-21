9f41e50e06e3fdb15247829b011d65e1f08c0c8f
Ben Webber <benjamin.webber@gmail.com>
2016-06-20 23:02:25 -0400

    Remove unused scripts

--- script/deps.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-IFS=$'\n\t'
-
-cd "$(dirname $(dirname "${BASH_SOURCE[0]}"))/src"
-
-lua ../script/soar.lua -a -s main.lua | cut -f1 | grep -Ev '(_main|binary dependencies)' | sort
-
-rm -f soar.out
