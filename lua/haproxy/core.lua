cd4d3c482328fdc15edc8501eafd0a64516dc5c9
Ben Webber <benjamin.webber@gmail.com>
2016-06-25 18:18:47 -0400

    Detect amalg in loaded modules

--- lua/haproxy/core.lua
+++ lua/haproxy/core.lua
@@ -3,8 +3,6 @@ local _core = core or {}
 -- Shared state between middleware.
 _core.ctx = {}
 
--- amalg: start
---
 -- Problem: amalg executes a script (`lua -l amalg`) to identify its
 -- dependencies However, HAProxy API functions are only available in an HAProxy
 -- execution context.
@@ -12,8 +10,7 @@ _core.ctx = {}
 -- Solution: stub a `core` table to satisfy execution at build time. The table's
 -- metatable will provide anonymous functions to satisfy calls like
 -- `core.register_service()`.
---
--- This block will be removed from the amalgamated output.
-setmetatable(_core, { __index = function() return function() return end end })
--- amalg: end
+if package.loaded['amalg'] then
+  setmetatable(_core, { __index = function() return function() return end end })
+end
 return _core
