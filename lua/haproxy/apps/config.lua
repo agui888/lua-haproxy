6eedb15605b4029f32690b400e9c69e542f74540
Ben Webber <benjamin.webber@gmail.com>
2016-06-26 16:04:45 -0400

    Use named constants for HTTP statuses

--- lua/haproxy/apps/config.lua
+++ lua/haproxy/apps/config.lua
@@ -1,12 +1,13 @@
 local core     = require('haproxy.core')
 local process  = require('haproxy.process')
+local http     = require('haproxy.embed.http')
 local Response = require('haproxy.embed.response')
 local View     = require('haproxy.embed.view')
 
 local ConfigView = View.new('ConfigView')
 
 function ConfigView:get(request, context)
-  return Response(200, core.ctx.config, { ['Content-Type'] = 'text/plain', })
+  return Response(http.status.OK, core.ctx.config, { ['Content-Type'] = 'text/plain', })
 end
 
 local function init()
--- lua/haproxy/apps/uname.lua
+++ lua/haproxy/apps/uname.lua
@@ -1,13 +1,14 @@
 local core     = require('haproxy.core')
 local util     = require('haproxy.util')
 
+local http     = require('haproxy.embed.http')
 local Response = require('haproxy.embed.response')
 local View     = require('haproxy.embed.view')
 
 local UnameView = View.new('UnameView')
 
 function UnameView:get(request, context)
-  return Response(200, core.ctx.uname, { ['Content-Type'] = 'text/plain', })
+  return Response(http.status.OK, core.ctx.uname, { ['Content-Type'] = 'text/plain', })
 end
 
 local function init()
